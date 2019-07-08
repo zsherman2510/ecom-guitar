528b4a42fbd47fafb90ba3f69a453a324ba6b2ad
zavion sherman <zsherman2510@hotmail.com>
2019-07-08 15:27:48 -0700

    before i started the login component

---
+++ client/src/components/Register_login/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import MyButton from "../utils/button";
+const RegisterLogin = () => {
+  return (
+    <div className="page_wrapper">
+      <div className="container">
+        <div className="register_login_container">
+          <div className="left">
+            <h1>New Customers</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              voluptatum, sapiente hic nesciunt minus earum similique quas
+              officiis nisi porro aperiam laborum maiores rerum et vel alias
+              itaque labore iste.Facere modi sapiente est nulla amet ducimus
+              alias. Sequi totam quas aut, eius modi quis. Vel esse, quaerat
+              vitae excepturi provident eos beatae. Hic sunt ipsam magnam,
+              officia quibusdam consequatur?
+            </p>
+            <MyButton
+              type="default"
+              title="Create an account"
+              linkTo="/register"
+              addStyles={{
+                margin: "10px 0 0 0"
+              }}
+            />
+          </div>
+          <div className="right">
+            <h2>Registered Customers</h2>
+            <p>If you have an account, Please log in!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterLogin;
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Layout from "./components/HOC/layout";
 import Home from "./components/Home";
+import RegisterLogin from "./components/Register_login";
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Layout>
